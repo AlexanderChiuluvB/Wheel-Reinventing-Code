fb7e79eb7391a3cb51fd1c4a59bb994b6859ea9d
AlexanderChiu <31793655+AlexanderChiuluvB@users.noreply.github.com>
2020-03-15 19:26:38 +0800

    Kappa

--- DAGStreaming/src/main/java/common/AbstractStreamService.java
+++ DAGStreaming/src/main/java/common/AbstractStreamService.java
@@ -1,9 +1,39 @@
 package common;
 
-public abstract class AbstractStreamService implements ServiceInterface {
+import java.util.List;
+
+public abstract class AbstractStreamService<I,O> implements ServiceInterface {
+
+    abstract protected void beforeStart() throws Exception;
+    abstract protected void beforeEnd() throws Exception;
+
+    abstract protected List<I> poll(List<Queue<I>> inputQueues) throws Exception;
+
+    abstract protected List<O> process(List<I> inputs) throws Exception;
+
+    abstract protected boolean offer(List<Queue<O>> outputQueues, List<O> outputs) throws Exception;
+
+    private List<ServiceInterface> upstreams;
+    private List<Queue<I>> inputQueues;
+    private List<Queue<O>> outputQueues;
+
+    private Thread thread;
+    private String name = this.toString();
+
+    protected volatile boolean stopped = false;
+    protected volatile boolean isShutdown = false;
+    private volatile ServiceStatus status = new ServiceStatus(stopped, isShutdown);
 
 
+    protected AbstractStreamService(String name,
+                                    List<ServiceInterface> upstreams,
+                                    List<Queue<I>> inputQueues,
+                                    List<Queue<O>> outputQueues){
+        this.name = name;
+        this.upstreams = upstreams;
+        this.inputQueues = inputQueues;
+        this.outputQueues = outputQueues;
 
-
+    }
 
 }
---
+++ Kappa/src/main/java/BatchLayer.java
@@ -0,0 +1,13 @@
+public class BatchLayer {
+
+    public static void main(String[] args) throws Exception {
+
+        final StreamExecutionEnvironemnt env = StreamExecutaion
+
+
+
+    }
+
+
+
+}
