30f5d847b81c1caee69a9a2985ac8f292ca4c60c
alex.zhao <alex.zhao@graviti.cn>
2020-03-26 21:02:12 +0800

    Client&Server

--- RPC/NettyExample/src/main/java/Client/EchoClientHandler.java
+++ RPC/NettyExample/src/main/java/Client/EchoClientHandler.java
@@ -1,4 +1,39 @@
 package Client;
 
-public class EchoClientHandler {
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    /**
+     * Called after the connection to the server is established
+     *
+     * @param ctx
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("连接服务器成功", CharsetUtil.UTF_8));
+    }
+
+    /**
+     * Called when a message is received from the server
+     *
+     * @param channelHandlerContext
+     * @param byteBuf
+     * @throws Exception
+     */
+    @Override
+    public void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println("Client received: " + byteBuf.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
 }
