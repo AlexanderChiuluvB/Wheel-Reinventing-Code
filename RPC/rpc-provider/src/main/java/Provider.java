326a9c37ba9770b5a8274b4c0b8ff09d0ba2481e
AlexanderChiuluvB <16307110258@fudan.edu.cn>
2020-04-23 12:30:46 +0800

    add rpc-provider

--- RPC/rpc-provider/src/main/java/Provider.java
+++ RPC/rpc-provider/src/main/java/Provider.java
@@ -1,2 +1,34 @@
-public class Provider {
+import com.damon.Configuration;
+import com.damon.URL;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import protocol.Protocol;
+import protocol.socket.SocketProtocol;
+
+public class Provider implements ApplicationContextAware {
+
+    private ApplicationContext ctx;
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("rpc.xml");
+        System.out.println(Configuration.getInstance().getAddress());
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+        this.ctx = ctx;
+
+        URL url = new URL("localhost", 8080);
+        //Register.register(url,HelloService.class.getName(),HelloServiceImpl.class.getName());
+
+        //启动 Tomcat
+        Protocol protocol = new SocketProtocol();
+        protocol.start(url);
+
+    }
+
+
 }
--- RPC/rpc-provider/src/test/java/ProviderTest.java
+++ RPC/rpc-provider/src/test/java/ProviderTest.java
@@ -1,5 +1,16 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 public class ProviderTest {
+
+    @Test
+    public void testIp() throws UnknownHostException {
+        InetAddress addr = InetAddress.getLocalHost();
+        System.out.println(addr.getHostAddress());
+    }
 
 }
